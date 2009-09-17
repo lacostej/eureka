ae4bf5afdad43eb5d704b65a9f690a2d8ca3ae51
jerome.lacoste <jerome.lacoste@gmail.com>
2009-09-17 12:49:16 +0200

    better cleanup script

--- clean.sh
+++ clean.sh
@@ -1,4 +1,4 @@
-rm *~
-rm *.pyc
-rm parser.out
+find . -name "*~" | xargs rm
+find . -name "*.pyc" | xargs rm
+rm -f parser.out
 grep "his file is automatically generated. Do not edit" *.py | cut -d ':' -f 1 | xargs rm
