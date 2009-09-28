b96375417b7ce4d68df640e58f6631803276798c
vgsn <vgsn@web74.webfaction.com>
2009-09-28 09:44:17 -0500

    Small script to auto_generate files when data changes

---
+++ vgsn_autogen.py
@@ -0,0 +1,35 @@
+import glob,os,stat,time
+import traceback
+
+import pdfsgen
+
+def checkSum():
+  '''Return a long which can be used to know if any .py files have changed.
+  Only looks in the current directory.'''
+  val = 0
+  for f in ['matte/interface.xls', 'matte/oppgaver4.txt']:
+    stats = os.stat (f)
+    val += stats [stat.ST_SIZE] + stats [stat.ST_MTIME]
+    return val
+
+def run():
+  val=0
+  newVal= 0
+  pwd = os.path.abspath(".")
+  while (True):
+#    print pwd
+    os.chdir(pwd)
+    try:
+      newVal = checkSum()
+      if newVal != val:
+        val = newVal
+        print "val " + str(val)
+        pdfsgen.main("matte/interface.xls", "matte/oppgaver4.txt")
+    except Exception, e:
+      print "ERROR: couldn't generate: " + str(e) 
+      traceback.print_exc()
+
+    time.sleep(1)
+
+if __name__ == '__main__':
+  run()
