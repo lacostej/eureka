f863757a98b0021449554d70e9999b9c752e777f
jerome.lacoste <jerome.lacoste@gmail.com>
2009-10-08 06:28:22 +0200

    don't have slashes in attachment names (manual test)

--- tests/mailertest.py
+++ tests/mailertest.py
@@ -1,6 +1,12 @@
 import mailer
 import sys
 from utils import *
+import os
 
-f = sys.argv[0]
-mailer.send_mail("eureka@vgsn.no", ["jerome.lacoste@gmail.com"], "Latest exercises from vgsn (7)", u(ur"Bla bla bla \u00f8 \u00d8"), [f], "smtp.gmail.com", "jbhkb.eureka", "jVsmpdg1*")
+files = [ sys.argv[0] ]
+other = "gen/pdfs/adeleide/adeleide.pdf"
+if os.access(other, os.F_OK):
+  files.append(other)
+
+mailer.send_mail("eureka@vgsn.no", ["jerome.lacoste@gmail.com"], "Latest exercises from vgsn (7)", u(ur"Bla bla bla \u00f8 \u00d8"), files, "smtp.gmail.com", "jbhkb.eureka", "jVsmpdg1*")
+
